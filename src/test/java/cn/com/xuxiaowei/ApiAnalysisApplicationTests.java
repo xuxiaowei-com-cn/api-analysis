a1ed4c86645424571480a8471c9e9b369150a4b2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-01-04 17:55:13 +0800

    init

---
+++ src/test/java/cn/com/xuxiaowei/ApiAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
